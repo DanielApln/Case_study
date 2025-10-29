01a2818b561bee6088f9b2cdb52bf8fee2417899
riichaes <cg.castillo.bsit2024@gmail.com>
2025-10-29 16:20:14 +0800

    Update Account.java

--- Account.java
+++ Account.java
@@ -86,14 +86,8 @@ class Account {
         // --- End Credential Setup ---
 
 
-
+      
       //gagawin ko toh 
-      //ang cute ko
-      //hello, world HAHAHA
-
-    boolean isValid = false;
-
-    while (!isValid) {
 
         System.out.print("Enter Pin code: ");
         pin = sc.nextLine();
@@ -101,65 +95,11 @@ class Account {
         System.out.print("Enter First Name: ");
         firstName = sc.nextLine();
 
-            	if (firstName == null || firstName.isEmpty()) {
-					throw new IllegalArgumentException("First Name cannot be empty. Please try again.");
-				}
-				
-				for (int i = 0; i < firstName.length(); i++) {
-			        char ch = firstName.charAt(i);
-			        
-			        if (Character.isLetter(ch)) {
-			        	continue;
-			        }
-			        else if(ch == ' ') {
-			        	continue;
-			        }
-			        else {
-			        	throw new IllegalArgumentException("First Name cannot contain any special character. Please try again."); 
-			        }
-			    }
-
         System.out.print("Enter Middle Name: ");
         middleName = sc.nextLine();
 
-            if(middleName == null || middleName.trim().isEmpty()) {
-				throw new IllegalArgumentException("Middle Name cannot be empty. Please try again.");
-			}
-			
-			for (int i = 0; i < middleName.length(); i++) {
-		        char ch = middleName.charAt(i);
-		        
-		        if (Character.isLetter(ch)) {
-		        	continue;
-		        }
-		        else if(ch == ' ') {
-		        	continue;
-		        }
-		        else {
-		        	throw new IllegalArgumentException("Middle Name cannot contain any special character. Please try again."); 
-		        }
-		    }
-
         System.out.print("Enter Last Name: ");
         lastName = sc.nextLine();
-
-            	if(lastName == null || lastName.trim().isEmpty()) {
-					throw new IllegalArgumentException("Last Name cannot be empty. Please try again.");
-				}
-				
-				for (int i = 0; i < lastName.length(); i++) {
-			        char ch = lastName.charAt(i);
-			        
-			        if (Character.isLetter(ch)) {
-			        	continue;
-			        }
-			        else if(ch == ' ') {
-			        	continue;
-			        }
-			        else {
-			        	throw new IllegalArgumentException("Last Name cannot contain any special character. Please try again."); 
-			        }
-			    }
 
         System.out.print("Enter Birthdate (YYYY-MM-DD): ");
         birthdate = sc.nextLine();
@@ -170,65 +110,11 @@ class Account {
         System.out.print("Enter Address: ");
         address = sc.nextLine();
 
-        	    if(address == null || address.trim().isEmpty()) {
-					throw new IllegalArgumentException("Address cannot be empty. Please try again.");
-				}
-				
-				for (int i = 0; i < address.length(); i++) {
-			        char ch = address.charAt(i);
-			        
-			        if (Character.isLetterOrDigit(ch)) {
-			        	continue;
-			        }
-			        else if(ch == ' ' || ch == '#' || ch == ',') {
-			        	continue;
-			        }
-			        else {
-			        	throw new IllegalArgumentException("Address cannot contain any special character. Please try again."); 
-			        }
-			    }
-
         System.out.print("Enter Father's Name: ");
         fatherName = sc.nextLine();
 
-                if(fatherName == null || fatherName.trim().isEmpty()) {
-					throw new IllegalArgumentException("Father's Name cannot be empty. Please try again.");
-				}
-				
-				for (int i = 0; i < fatherName.length(); i++) {
-			        char ch = fatherName.charAt(i);
-			        
-			        if (Character.isLetter(ch)) {
-			        	continue;
-			        }
-			        else if(ch == ' ') {
-			        	continue;
-			        }
-			        else {
-			        	throw new IllegalArgumentException("Father's Name cannot contain any special character. Please try again."); 
-			        }
-			    }
-
         System.out.print("Enter Mother's Name: ");
         motherName = sc.nextLine();
-
-                if(motherName == null || motherName.trim().isEmpty()) {
-					throw new IllegalArgumentException("Mother's Name cannot be empty. Please try again.");
-				}
-				
-				for (int i = 0; i < motherName.length(); i++) {
-			        char ch = motherName.charAt(i);
-			        
-			        if (Character.isLetter(ch)) {
-			        	continue;
-			        }
-			        else if(ch == ' ') {
-			        	continue;
-			        }
-			        else {
-			        	throw new IllegalArgumentException("Mother's Name cannot contain any special character. Please try again."); 
-			        }
-			    }
 
         System.out.print("Enter Contact No: ");
         contactNo = sc.nextLine();
@@ -237,15 +123,5 @@ class Account {
         System.out.println("\nRegistration Successful! Your Account Number is: " + accountNo);
 
         saveToFile();
-
-        		isValid = true;
-				System.out.println("\n=== Account Information Successfully Added! ===");
-				System.out.println("First Name: " + firstName.toUpperCase());
-				System.out.println("Middle Name: " + middleName.toUpperCase());
-				System.out.println("Last Name: " + lastName.toUpperCase());
-				System.out.println("Father's Name: " + fatherName.toUpperCase());
-				System.out.println("Mother's Name: " + motherName.toUpperCase());
-				System.out.println("Address: " + address.toUpperCase());
-		}
     }
 }
